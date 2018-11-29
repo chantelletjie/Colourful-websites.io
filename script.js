e4b4a3919d5e988db727da26fa58eb367cbde48f
Chantelle Landman <chantellelandman710@gmail.com>
2018-11-29 11:11:19 +0200

    combination of particles and linear backgrounds

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
+var color3 = document.querySelector(".color3");
 var body = document.getElementById("gradient");
 
 function setGradient() {
@@ -8,8 +9,11 @@ function setGradient() {
     + color1.value 
     + ","
     + color2.value 
+    + ","
+    + color3.value 
     + ")";
 }
 
 color1.addEventListener("input" , setGradient);
 color2.addEventListener("input" , setGradient);
+color3.addEventListener("input" , setGradient);
